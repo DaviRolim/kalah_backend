22298f275ee6800efd2b7bfe452e9dd64e829255
Davi Rolim <davirolim94@gmail.com>
2021-11-25 21:41:53 -0300

    fixing the error and adding reports (firsttry)

--- src/test/java/com/github/kalaha/kalaha_game/KalahaGameApplicationTests.java
+++ src/test/java/com/github/kalaha/kalaha_game/KalahaGameApplicationTests.java
@@ -17,7 +17,7 @@ class KalahaGameApplicationTests {
 	@Test
 	void firstPlayerToPlayShouldBePlayer1() {
 		board.initialize();	
-		assertEquals(board.getCurrentPlayerAsString(), "Player2");
+		assertEquals(board.getCurrentPlayerAsString(), "Player1");
 
 	}
 
